2d0497699910548596be9f41dd2f76a0e38fbeea
OKI-2024 <tetsuokinokun@gmail.com>
2024-04-04 14:45:37 +0900

    レビュー投稿を少々（ルーティング/Newアクション）

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "reviews#index"
-  resources :reviews, only: :index
+  resources :reviews, only: [:index, :new]
   resources :users, only: [:edit, :update]
 
 end
