fd3ec009af87e23204882fd7690a9b34507bcdbf
OKI-2024 <tetsuokinokun@gmail.com>
2024-04-05 18:10:04 +0900

    産地のプルダウン選択を実装

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -5,7 +5,20 @@ end
 
 def new
   @review = Review.new
+  @grouped_wine_regions = grouped_wine_regions
 end
 
+
+private
+
+def grouped_wine_regions
+  wine_regions_hash = {}
+
+  WineRegion.roots.each do |parent_region|
+    wine_regions_hash[parent_region.name] = parent_region.descendants.pluck(:name)
+  end
+
+  wine_regions_hash
+end
 
 end
