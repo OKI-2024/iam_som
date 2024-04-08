0f100c30fce190dd97a15fc98635bdd66cfa0de6
OKI-2024 <tetsuokinokun@gmail.com>
2024-04-08 11:33:54 +0900

    reviews#updateのルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "reviews#index"
-  resources :reviews, only: [:index, :new]
+  resources :reviews, only: [:index, :new, :update]
   resources :users, only: [:edit, :update]
 
 end
