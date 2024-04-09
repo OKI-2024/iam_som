1f9f320882875c329fcb6b154f5378151769d48b
OKI-2024 <tetsuokinokun@gmail.com>
2024-04-09 12:44:21 +0900

    投稿→データ保存完了（fragrances以外）

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,12 +2,9 @@ class Review < ApplicationRecord
   has_one_attached :image
   belongs_to :user
   belongs_to :wine
-  
-
- 
-
 
   validates_presence_of :sweetness, :bitterness, :acidity, :alcohol
+  serialize :fragrances, Array
 
   def self.wine_attributes
     %i[sweetness bitterness acidity alcohol]
--- app/models/wine.rb
+++ app/models/wine.rb
@@ -1,8 +1,5 @@
 class Wine < ApplicationRecord
   has_many :reviews
-  belongs_to :grape_variety
-  belongs_to :type
-  belongs_to :region
 
   validates :grape_variety_id, numericality: { other_than: 1 }
   validates :type_id, numericality: { other_than: 1 }
