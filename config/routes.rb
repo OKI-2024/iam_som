224a8febce083e67dab95e4b089d4605f5cea500
OKI-2024 <tetsuokinokun@gmail.com>
2024-04-10 18:12:18 +0900

    ワイン詳細ページ作成＆画像無し投稿可

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "reviews#index"
-  resources :wines, only: :[:show] do
-    resources :reviews, only: [:index, :new, :create] end
+  resources :wines, only: [:show]
+  resources :reviews, only: [:index, :new, :create, :destroy]
   resources :users, only: [:edit, :update]
 
 
