0811b05f06311359f6c8389d811988bf81da15e3
OKI-2024 <tetsuokinokun@gmail.com>
2024-04-12 20:01:34 +0900

    シンプル検索OK

--- app/controllers/wines_controller.rb
+++ app/controllers/wines_controller.rb
@@ -6,11 +6,24 @@ def show
 end
 
 def search
-  @q = Wine.ransack(params[:q])
-  @wine= @q.result
+  keyword = params[:keyword]
+
+  @wines = Wine.joins(:reviews)
+               .where("wines.name LIKE :keyword OR wines.producer LIKE :keyword OR reviews.content LIKE :keyword", keyword: "%#{keyword}%")
+               .distinct
+
+  if params[:types].present?
+    @wines = @wines.where(type_id: params[:types])
+  end
+
+  if params[:regions].present?
+    @wines = @wines.where(region_id: params[:regions])
+  end
+
+  if params[:grape_varieties].present?
+    @wines = @wines.where(grape_variety_id: params[:grape_varieties])
+  end
 end
-
-
 
 
 
