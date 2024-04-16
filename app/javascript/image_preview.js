dfe8d665c17645b99cc2c729d92e56268fe40d81
OKI-2024 <tetsuokinokun@gmail.com>
2024-04-16 23:37:09 +0900

    レビュー投稿画面・検索結果一覧画面を修正

---
+++ app/javascript/image_preview.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function() {
+  const imageInput = document.getElementById('image');
+  const preview = document.getElementById('preview');
+
+  imageInput.addEventListener('change', function(event) {
+    const file = event.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = function(e) {
+        preview.src = e.target.result;
+        preview.style.display = 'inline-block'; // この行を変更
+      };
+      reader.readAsDataURL(file);
+    }
+  });
+});
