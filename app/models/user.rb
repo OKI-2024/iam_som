b7b671d6e179989a064dce55e4cab39b2e8889d2
OKI-2024 <tetsuokinokun@gmail.com>
2024-04-17 17:33:59 +0900

    バリデーションを記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,13 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :reviews
   has_one_attached :avatar
+  
+  
+  validates :nickname, presence: { message: "ニックネームは必須です" },
+                       length: { maximum: 10, too_long: "ニックネームは最大%{count}文字までです" },
+                       uniqueness: { message: "このニックネームは既に使用されています" }
+
+  validates :introduction, length: { maximum: 240, too_long: "自己紹介は最大%{count}文字までです" },
+                           allow_blank: true
+
 end
