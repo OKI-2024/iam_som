f307b1ae254a6128c1059a44e39fb2ad22186bb2
OKI-2024 <tetsuokinokun@gmail.com>
2024-04-17 17:55:37 +0900

    Userモデルのエラーハンドリング設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,17 +1,18 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-  has_many :reviews
-  has_one_attached :avatar
-  
-  
-  validates :nickname, presence: { message: "ニックネームは必須です" },
-                       length: { maximum: 10, too_long: "ニックネームは最大%{count}文字までです" },
-                       uniqueness: { message: "このニックネームは既に使用されています" }
-
-  validates :introduction, length: { maximum: 240, too_long: "自己紹介は最大%{count}文字までです" },
-                           allow_blank: true
-
-end
+       # Include default devise modules. Others available are:
+       # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+       devise :database_authenticatable, :registerable,
+              :recoverable, :rememberable, :validatable
+       has_many :reviews
+       has_one_attached :avatar
+       
+       # ニックネームのバリデーション
+       validates :nickname, presence: true,
+                            length: { maximum: 10 },
+                            uniqueness: true
+     
+       # 自己紹介のバリデーション
+       validates :introduction, length: { maximum: 240 },
+                                allow_blank: true
+     end
+     
--- config/application.rb
+++ config/application.rb
@@ -8,16 +8,22 @@ Bundler.require(*Rails.groups)
 
 module IamSom
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 7.0
-    config.active_storage.variant_processor = :mini_magick  
-
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
+     # 最初に生成されたRailsバージョンのデフォルト設定を初期化します。
+     config.load_defaults 7.0
+     config.active_storage.variant_processor = :mini_magick  
+ 
+     # アプリケーション、エンジン、およびrailtiesの設定はここに記述します。
+     #
+     # これらの設定は、特定の環境のconfig/environments内のファイルを使用して、
+     # 後で上書きすることができます。
+     #
+     # config.time_zone = "Tokyo"
+     # config.i18n.default_locale = :ja
+ 
+     # 日本語をデフォルトの言語として設定
+     config.i18n.default_locale = :ja
+ 
+     # ロケールファイルのパスを指定
+     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
   end
 end
